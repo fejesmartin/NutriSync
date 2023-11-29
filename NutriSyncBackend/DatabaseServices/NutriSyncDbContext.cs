1a5fcf60479f07e5c716fedfd12ee9e734a5cde5
fejesmartin <fejes.martin00@gmail.com>
2023-11-29 10:39:09 +0100

    feat: add DBContext

---
+++ NutriSyncBackend/DatabaseServices/NutriSyncDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NutriSyncBackend.Models;
+
+namespace NutriSyncBackend.DatabaseServices;
+
+public class NutriSyncDbContext: IdentityDbContext<User,IdentityRole,string>
+{
+    public NutriSyncDbContext(DbContextOptions<NutriSyncDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<User> Users { get; set; }
+}
