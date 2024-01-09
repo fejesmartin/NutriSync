136d273f384d92dd090fa9123d68c6989182383e
fejesmartin <fejes.martin00@gmail.com>
2024-01-09 15:15:38 +0100

    feature: adds DBContext

--- NutriSyncBackend/DatabaseServices/NutriSyncDbContext.cs
+++ NutriSyncBackend/DatabaseServices/NutriSyncDbContext.cs
@@ -3,14 +3,33 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using NutriSyncBackend.Models;
 
-namespace NutriSyncBackend.DatabaseServices;
-
-public class NutriSyncDbContext: IdentityDbContext<User,IdentityRole,string>
+namespace NutriSyncBackend.DatabaseServices
 {
-    public NutriSyncDbContext(DbContextOptions<NutriSyncDbContext> options) : base(options)
+    #region NutriSyncDbContext
+
+    // NutriSyncDbContext class that extends IdentityDbContext with User and IdentityRole
+    public class NutriSyncDbContext : IdentityDbContext<User, IdentityRole, string>
     {
-        
+        // DbSet for the User entity
+        public DbSet<User> Users { get; set; }
+
+        // Constructor for NutriSyncDbContext
+        public NutriSyncDbContext()
+        {
+            // Apply database migration if it's a relational database
+            if (Database.IsRelational()) Database.Migrate();
+        }
+
+        // Override method to configure database connection
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Get connection string from environment variables
+            var connectionString = Environment.GetEnvironmentVariable("ASPNETCORE_CONNECTIONSTRING");
+
+            // Use SQL Server with the provided connection string
+            optionsBuilder.UseSqlServer(connectionString);
+        }
     }
-    
-    public DbSet<User> Users { get; set; }
+
+    #endregion
 }
