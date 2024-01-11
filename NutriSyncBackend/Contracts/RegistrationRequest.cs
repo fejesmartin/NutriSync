12f010046e6cec12cb9e8e27818c6f91ec380778
fejesmartin <fejes.martin00@gmail.com>
2024-01-11 18:20:08 +0100

    feature: implements Registration Response record

---
+++ NutriSyncBackend/Contracts/RegistrationRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NutriSyncBackend.Contracts;
+
+public record RegistrationRequest( 
+    [Required]string Email, 
+    [Required]string Username, 
+    [Required]string Password
+    );
