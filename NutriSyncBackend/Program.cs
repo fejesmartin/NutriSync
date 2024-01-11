dc89da20f565eb25a3e74631fa004dc148838bf8
fejesmartin <fejes.martin00@gmail.com>
2024-01-11 18:22:28 +0100

    feature: updates Program class

--- NutriSyncBackend/Program.cs
+++ NutriSyncBackend/Program.cs
@@ -2,27 +2,74 @@ using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
-using NutriSyncBackend.DatabaseServices;
-using NutriSyncBackend.Models;
+using Microsoft.OpenApi.Models;
+using NutriSyncBackend.Authentication;
+using NutriSyncBackend.Authentication.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 #region ConfigureServices
 
-builder.Services.AddDbContext<NutriSyncDbContext>();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy
+                .SetIsOriginAllowed(_ => true)
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        });
+});
 
-builder.Services.AddIdentity<User, IdentityRole>()
-    .AddEntityFrameworkStores<NutriSyncDbContext>()
-    .AddDefaultTokenProviders();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 
+// Add your custom services
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+// Add Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "NutriSync API", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
+// Add DbContext
+builder.Services.AddDbContext<UserContext>();
+
+// Add authentication and JWT Bearer options
 builder.Services.AddAuthentication(options =>
 {
-    // Set the default authentication scheme to JwtBearer
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 }).AddJwtBearer(options =>
 {
-    // Configure JWT Bearer authentication options
     options.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuer = true,
@@ -35,12 +82,40 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
+// Add Identity with UserManager and RoleManager
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = false;
+        options.User.RequireUniqueEmail = true;
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 6;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireLowercase = false;
+    })
+    .AddEntityFrameworkStores<UserContext>()
+    .AddDefaultTokenProviders();
+
+
 #endregion
 
 var app = builder.Build();
 
 #region Run
 
+app.UseCors();
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+
+    // Enable Swagger UI
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NutriSync API v1"));
+}
 app.Run();
 
 #endregion
