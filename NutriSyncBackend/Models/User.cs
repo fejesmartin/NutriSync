606127be09e4f67da0cc7934e6ed1f9ad023b128
fejesmartin <fejes.martin00@gmail.com>
2024-01-22 07:53:55 +0100

    chore: delete UserProfile model and UserContext class

--- NutriSyncBackend/Models/User.cs
+++ NutriSyncBackend/Models/User.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
 
 namespace NutriSyncBackend.Models;
 
 public class User: IdentityUser
 {
-    public UserProfile? UserProfile { get; set; }
-    public int ProfileId { get; set; }
+    public string UserId { get; set; } 
+    public string UserName { get; set; }
+    public byte[]? ProfilePicture { get; set; }
+    public string DisplayName { get; set; }
+    public List<Product> BoughtProducts { get; set; }
+    public List<Plan> Plans { get; set; }
 }
