15572ef76c7f2422135b129c263d83517a94f813
fejesmartin <fejes.martin00@gmail.com>
2024-01-22 07:54:48 +0100

    feature: implement IRepository interface

---
+++ NutriSyncBackend/Context/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace NutriSyncBackend.Context.Repositories;
+
+public interface IRepository<T> where T : class
+{
+    IEnumerable<T> GetAll();
+    T? GetById(int id);
+    void Create(T obj);
+    void Update(T obj);
+    void Delete(int id);
+}
