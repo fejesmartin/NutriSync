9b687666e7e91f105ab1a03995e078c0b4e6d64f
fejesmartin <fejes.martin00@gmail.com>
2024-01-22 07:56:42 +0100

    feature: implement Product controller

---
+++ NutriSyncBackend/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NutriSyncBackend.Context.Repositories;
+
+namespace NutriSyncBackend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductController: ControllerBase
+{
+    private readonly PlanRepository _planRepository;
+    
+    public ProductController(PlanRepository planRepository)
+    {
+        _planRepository = planRepository;
+    }
+
+    public IActionResult GetAllProducts()
+    {
+        var products = _planRepository.GetAll();
+        return Ok(products);
+    }
+}
