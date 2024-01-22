28c6c8d6e0c5748c79320beda69a7539128b562a
fejesmartin <fejes.martin00@gmail.com>
2024-01-22 07:55:28 +0100

    feature: add Plan model

---
+++ NutriSyncBackend/Models/Plan.cs
@@ -0,0 +1,16 @@
+﻿namespace NutriSyncBackend.Models;
+
+public class Plan
+{
+    public int PlanId { get; set; }
+    public string PlanName { get; set; }
+    public DateTime CreatedAt = DateTime.Now;
+    public string Description { get; set; }
+    public int CalorieIn { get; set; }
+    public int CalorieMax { get; set; }
+    // Foreign key
+    public int UserId { get; set; }
+
+    // Navigation property
+    public User User { get; set; }
+}
