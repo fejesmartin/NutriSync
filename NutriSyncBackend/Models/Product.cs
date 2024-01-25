4b2f8176d79e6966992b05e8770d2441a83a1061
fejesmartin <fejes.martin00@gmail.com>
2024-01-25 17:49:49 +0100

    fix: Product model

--- NutriSyncBackend/Models/Product.cs
+++ NutriSyncBackend/Models/Product.cs
@@ -1,14 +1,15 @@
-﻿namespace NutriSyncBackend.Models;
-
+﻿
 public class Product
 {
     public int ProductId { get; set; }
     public string ProductName { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
-    // Foreign key
-    public int UserId { get; set; }
+    public byte[] ImageData { get; set; }
+    public string ImageMimeType { get; set; }
+    public string UserId { get; set; }
 
     // Navigation property
     public User User { get; set; }
+    
 }
