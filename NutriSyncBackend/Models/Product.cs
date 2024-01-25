1e88831ece8910e51cd0c4845468ef367ebc895e
fejesmartin <fejes.martin00@gmail.com>
2024-01-25 22:34:14 +0100

    fix: Product model

--- NutriSyncBackend/Models/Product.cs
+++ NutriSyncBackend/Models/Product.cs
@@ -5,8 +5,6 @@ public class Product
     public string ProductName { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
-    public byte[] ImageData { get; set; }
-    public string ImageMimeType { get; set; }
     public string UserId { get; set; }
 
     // Navigation property
