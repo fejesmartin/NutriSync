44d3331b8f8119f8ec85141f209baf9d14b1746c
fejesmartin <fejes.martin00@gmail.com>
2024-01-25 17:49:32 +0100

    fix: Plan model

--- NutriSyncBackend/Models/Plan.cs
+++ NutriSyncBackend/Models/Plan.cs
@@ -1,16 +1,19 @@
-﻿namespace NutriSyncBackend.Models;
-
+﻿
 public class Plan
 {
     public int PlanId { get; set; }
     public string PlanName { get; set; }
-    public DateTime CreatedAt = DateTime.Now;
+    public DateTime CreatedAt { get; set; }
     public string Description { get; set; }
     public int CalorieIn { get; set; }
     public int CalorieMax { get; set; }
-    // Foreign key
-    public int UserId { get; set; }
+    public string UserId { get; set; }
 
     // Navigation property
     public User User { get; set; }
+
+    public Plan()
+    {
+        CreatedAt = DateTime.Now;
+    }
 }
