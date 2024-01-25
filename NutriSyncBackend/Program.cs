9aaf3aa53d66a747340ba4e1d84092a795086916
fejesmartin <fejes.martin00@gmail.com>
2024-01-25 22:35:05 +0100

    fix: Program class

--- NutriSyncBackend/Program.cs
+++ NutriSyncBackend/Program.cs
@@ -1,35 +1,46 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using NutriSyncBackend.Authentication;
+using NutriSyncBackend.Context.Repositories;
+
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-#region ConfigureServices
-
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(
-        policy =>
-        {
-            policy
-                .SetIsOriginAllowed(_ => true)
-                .AllowAnyHeader()
-                .AllowAnyMethod()
-                .AllowCredentials();
-        });
-});
-
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
-// Add your custom services
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddScoped<IAuthService, AuthService>();
+// Add DbContext
+builder.Services.AddDbContext<NutriSyncDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// Add authentication and JWT Bearer options
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["NUTRISYNC_JWT_ISSUER_SIGNING_KEY"])),
+            ValidIssuer = builder.Configuration["NUTRISYNC_JWT_VALID_ISSUER"],
+            ValidAudience = builder.Configuration["NUTRISYNC_JWT_VALID_AUDIENCE"]
+        };
+    });
+
+// Add Identity with UserManager and RoleManager
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<NutriSyncDBContext>()
+    .AddDefaultTokenProviders();
 
 // Add Swagger
 builder.Services.AddSwaggerGen(c =>
@@ -60,62 +71,24 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
-// Add DbContext
-builder.Services.AddDbContext<NutriSyncDBContext>();
-
-// Add authentication and JWT Bearer options
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("NUTRISYNC_JWT_ISSUER_SIGNING_KEY"))),
-        ValidIssuer = Environment.GetEnvironmentVariable("NUTRISYNC_JWT_VALID_ISSUER"),
-        ValidAudience = Environment.GetEnvironmentVariable("NUTRISYNC_JWT_VALID_AUDIENCE")
-    };
-});
-
-// Add Identity with UserManager and RoleManager
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
-    {
-        options.SignIn.RequireConfirmedAccount = false;
-        options.User.RequireUniqueEmail = true;
-        options.Password.RequireDigit = false;
-        options.Password.RequiredLength = 6;
-        options.Password.RequireNonAlphanumeric = false;
-        options.Password.RequireUppercase = false;
-        options.Password.RequireLowercase = false;
-    })
-    .AddEntityFrameworkStores<NutriSyncDBContext>()
-    .AddDefaultTokenProviders();
-
-
-#endregion
-
 var app = builder.Build();
 
-#region Run
-
+// Middleware pipeline
 app.UseCors();
 app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NutriSync API v1"));
+app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-
-    // Enable Swagger UI
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NutriSync API v1"));
 }
-app.Run();
 
-#endregion
+app.Run();
