607741c25e85673c0ffd1ad17ec511e6f5e1fff5
fejesmartin <fejes.martin00@gmail.com>
2024-02-22 14:20:52 +0100

    feature: implement improved and fully remade login page

--- NutriSyncFrontend/frontend/src/Components/LoginComponent.tsx
+++ NutriSyncFrontend/frontend/src/Components/LoginComponent.tsx
@@ -1,14 +1,47 @@
-import React, { useState } from "react";
-import { Form, Button, Container } from "react-bootstrap";
-import { LoginFormData } from "../Models/LoginFormData";
+
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import LoginPic from '../Images/LoginPic.png'
+import { Navigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import backendUrl from "../Secrets/backendurl";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 import Cookies from "universal-cookie";
 import { jwtDecode, JwtPayload } from "jwt-decode";
 import { User } from "../Models/User";
+import { useState } from "react";
+import { LoginFormData } from "../Models/LoginFormData";
 
-const LoginComponent: React.FC = () => {
+function Copyright(props: any) {
+ 
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        NutriSync
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function SignInSide() {
   const navigate = useNavigate();
   const cookies = new Cookies();
 
@@ -57,45 +90,97 @@ const LoginComponent: React.FC = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    console.log(formData);
     loginUser(formData);
   };
 
   return (
-    <Container className="d-flex justify-content-center align-items-center ">
-      <Form className="w-50" onSubmit={handleSubmit}>
-        <h2 className="mb-3 text-center">Login</h2>
-        <Form.Group className="mb-3" controlId="formEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </Form.Group>
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+  item
+  xs={false}
+  sm={4}
+  md={7}
+  sx={{
+    backgroundImage: `url(${LoginPic})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundColor: (t: any) =>
+      t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center top', 
+  }}
+/>
 
-        <Form.Group className="mb-3" controlId="formPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </Form.Group>
-
-        <div className="text-center">
-          <Button variant="primary" type="submit">
-            Login
-          </Button>
-        </div>
-      </Form>
-    </Container>
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                onChange={handleChange}
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={handleChange}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2" onClick={()=> navigate("/register")}>
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
-};
-
-export default LoginComponent;
+}
