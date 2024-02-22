adb98c10c5b8112d2ec2c62255fbfa46cb0d2956
fejesmartin <fejes.martin00@gmail.com>
2024-02-22 14:19:28 +0100

    feature: add additional properties and function to User model

--- NutriSyncFrontend/frontend/src/Models/User.tsx
+++ NutriSyncFrontend/frontend/src/Models/User.tsx
@@ -2,9 +2,21 @@ export class User {
     userName: string;
     email: string;
     token: string;
-    constructor(username: string, email: string, token: string){
+    firstName: string | null;
+    lastName: string | null;
+    profilePictureUrl: string | null;
+
+    constructor(username: string, email: string, token: string, firstName: string | null = null, lastName: string | null = null, profilePictureUrl: string | null = null){
         this.userName = username;
         this.email = email;
         this.token = token;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.profilePictureUrl = profilePictureUrl;
+    }
+
+    updateProfilePicture(profilePictureUrl: string | null): void {
+        this.profilePictureUrl = profilePictureUrl;
+        localStorage.setItem('profilePictureUrl', profilePictureUrl || '');
     }
 }
