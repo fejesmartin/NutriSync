706575d55cd8744e5057d6f1b98af1387e3423e8
fejesmartin <fejes.martin00@gmail.com>
2024-02-22 09:54:41 +0100

    feature: add Layout style

--- NutriSyncFrontend/frontend/src/Pages/Layout.tsx
+++ NutriSyncFrontend/frontend/src/Pages/Layout.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Container, Row } from "react-bootstrap";
 import Navigationbar from "../Components/Navigationbar";
+import "../Styles/Layout.css";
 
 const Layout: React.FC = () => {
   return (
-    <Container fluid className="min-vh-100">
-      <Row className="h-10"><Navigationbar/></Row>
+    <Container fluid className="min-vh-100 main">
+      <Row className="h-10">
+        <Navigationbar />
+      </Row>
       <Row className="flex-grow-1">
         <Outlet />
       </Row>
