488abbb35a34a4caf41963a62e4890c2fabb23c7
fejesmartin <fejes.martin00@gmail.com>
2024-02-24 12:20:34 +0100

    feature: implement logger

---
+++ NutriSyncBackend/Logger/ILogger.cs
@@ -0,0 +1,7 @@
+﻿namespace NutriSyncBackend.Logger;
+
+public interface ILogger
+{
+    void LogInformation(string message);
+    void LogError(string message, Exception ex);
+}
---
+++ NutriSyncBackend/Logger/Logger.cs
@@ -0,0 +1,22 @@
+﻿namespace NutriSyncBackend.Logger;
+using Microsoft.Extensions.Logging;
+
+public class ConsoleLogger : ILogger
+{
+    private readonly ILogger<ConsoleLogger> _logger;
+
+    public ConsoleLogger(ILogger<ConsoleLogger> logger)
+    {
+        _logger = logger;
+    }
+
+    public void LogInformation(string message)
+    {
+        _logger.LogInformation($"INFO: {message}");
+    }
+
+    public void LogError(string message, Exception ex)
+    {
+        _logger.LogError(ex,$"ERROR: {message}");
+    }
+}
