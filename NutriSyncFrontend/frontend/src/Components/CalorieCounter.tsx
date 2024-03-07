44c4d6dc178d62fcd20c6e10da30627e7719080b
fejesmartin <fejes.martin00@gmail.com>
2024-03-07 11:09:00 +0100

    feature: improve Calorie Counter page

--- NutriSyncFrontend/frontend/src/Components/CalorieCounter.tsx
+++ NutriSyncFrontend/frontend/src/Components/CalorieCounter.tsx
@@ -1,35 +1,33 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Container, Row, Col, Alert, Spinner } from 'react-bootstrap';
 import APIKey from '../Secrets/APIKey';
+import CaloriePic from '../Images/caloriecounter.jpg'
 
-interface CalorieCheckResponse {
-    
-          name: string,
-          calories: number,
-          serving_size_g: number,
-          fat_total_g: number,
-          fat_saturated_g: number,
-          protein_g: number,
-          sodium_mg: number,
-          potassium_mg: number,
-          cholesterol_mg: number,
-          carbohydrates_total_g: number,
-          fiber_g: number,
-          sugar_g: number
-    
+interface NutritionData {
+  name: string;
+  calories: number;
+  serving_size_g: number;
+  fat_total_g: number;
+  fat_saturated_g: number;
+  protein_g: number;
+  sodium_mg: number;
+  potassium_mg: number;
+  cholesterol_mg: number;
+  carbohydrates_total_g: number;
+  fiber_g: number;
+  sugar_g: number;
 }
-
-interface ArrayCalorieCheckResponse extends Array<CalorieCheckResponse> {}
 
 const CalorieCounter: React.FC = () => {
   const [itemName, setItemName] = useState<string>('');
-  const [calories, setCalories] = useState<number | null>(null);
+  const [nutritionData, setNutritionData] = useState<NutritionData | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
   const baseURL: string = "https://api.api-ninjas.com/v1/nutrition?query=";
 
-
   const handleCheckCalories = async () => {
+    setLoading(true);
     try {
-      
       const response = await fetch(`${baseURL + itemName}`, {
         method: 'GET',
         headers: {
@@ -39,39 +37,78 @@ const CalorieCounter: React.FC = () => {
       });
 
       if (response.ok) {
-        const data: ArrayCalorieCheckResponse = await response.json();
-        
-        setCalories(data[0].calories);
+        const data: NutritionData[] = await response.json();
+        setLoading(false); // Set loading to false when fetching is completed
+        if (data.length > 0) {
+          setNutritionData(data[0]);
+          setError(null);
+        } else {
+          setNutritionData(null);
+          setError('No data found for the item.');
+        }
       } else {
-        console.error('Error fetching calorie information');
+        setError('Error fetching calorie information');
       }
-    } catch (error) {
-      console.error('Error fetching calorie information:', error);
+    } catch (error: any) {
+      setError('Error fetching calorie information: ' + error.message);
     }
   };
 
   return (
-    <div>
-      <Form>
-        <Form.Group controlId="itemName">
-          <Form.Label>Item Name:</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter item name"
-            value={itemName}
-            onChange={(e) => setItemName(e.target.value)}
-          />
-        </Form.Group>
-        <Button variant="primary" onClick={handleCheckCalories}>
-          Check Calories
-        </Button>
-      </Form>
-      {calories && (
-        <div>
-          <p>Calories for {itemName}: {calories} cal</p>
-        </div>
-      )}
-    </div>
+    <Container fluid>
+      <Row>
+        <Col md={6} style={{ backgroundImage: `url(${CaloriePic})`, backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh' }}>
+        </Col>
+        <Col md={6}>
+          <Container className="h-100">
+            <Row className="justify-content-center align-items-center h-100">
+              <Col md={8}>
+                <Form>
+                  <Form.Group controlId="itemName">
+                  <h1>Calorie Counter</h1>
+                    <Form.Label>Item Name:</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter item name"
+                      value={itemName}
+                      onChange={(e) => setItemName(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Button variant="primary" onClick={handleCheckCalories}>
+                    Check Nutrition
+                  </Button>
+                </Form>
+                {error && <Alert variant="danger">{error}</Alert>}
+                {loading && (
+                  <div className="text-center mt-4">
+                    <Spinner animation="border" variant="primary" />
+                  </div>
+                )}
+                {nutritionData && !loading && (
+                  <div className="mt-4">
+                   
+                    <h5>Nutrition Information for {itemName}</h5>
+                    <ul>
+                      <li>Calories: {nutritionData.calories} cal</li>
+                      <li>Serving Size: {nutritionData.serving_size_g} g</li>
+                      <li>Total Fat: {nutritionData.fat_total_g} g</li>
+                      <li>Saturated Fat: {nutritionData.fat_saturated_g} g</li>
+                      <li>Protein: {nutritionData.protein_g} g</li>
+                      <li>Sodium: {nutritionData.sodium_mg} mg</li>
+                      <li>Potassium: {nutritionData.potassium_mg} mg</li>
+                      <li>Cholesterol: {nutritionData.cholesterol_mg} mg</li>
+                      <li>Total Carbohydrates: {nutritionData.carbohydrates_total_g} g</li>
+                      <li>Fiber: {nutritionData.fiber_g} g</li>
+                      <li>Sugar: {nutritionData.sugar_g} g</li>
+                    </ul>
+                  </div>
+                )}
+              </Col>
+            </Row>
+          </Container>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
